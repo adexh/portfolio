c4a890d7351458626366f062c7626ca02199770d
Adesh Tamrakar <adesht@outlook.com>
2024-03-03 22:52:34 +0530

    added images to project

--- src/app/_components/projects.tsx
+++ src/app/_components/projects.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import { PinContainer } from "@/components/ui/3d-pin";
+import Image from "next/image";
 
 export function Projects() {
   const projects = [
@@ -9,19 +10,21 @@ export function Projects() {
       description: "This application helps any small enterprise manage their projects, clients and employee details.",
       hrefTitle: "github.com/NextERP",
       href: "https://github.com/adexh/NextERP",
-      image:""
+      image:"nexterp.png"
     },
     {
       title: "Course website",
       description: "Course selling website, with home page, course view page and checkout.",
       hrefTitle: "github.com/simple-course-app",
       href: "https://github.com/adexh/simple-course-app",
+      image:"course.png"
     },
     {
       title: "Portfolio",
-      description: "",
+      description: "Portfolio website built using NextJs and Aceternity UI",
       hrefTitle: "github.com/portfolio",
       href: "https://github.com/adexh/portfolio",
+      image:"portfolio.png"
     }
   ]
 
@@ -37,14 +40,14 @@ export function Projects() {
         </div>
       </h3>
 
-      <div className="grid grid-cols-1 mb-20 md:grid-cols-4 sm:grid-cols-2">
+      <div className="grid grid-cols-1 mb-20 md:grid-cols-4 sm:grid-cols-2 gap-y-6">
         {projects.map((project, index) => (
           <div key={index} className="">
             <PinContainer
               title={project.hrefTitle}
               href={project.href}
             >
-              <div className="flex flex-col p-4 tracking-tight text-slate-100/50 min-w-[20rem] h-[20rem] border border-white/[0.1]">
+              <div className="flex flex-col justify-between p-4 tracking-tight text-slate-100/50 min-w-[20rem] h-[20rem] border rounded-xl">
                 <h3 className="max-w-xs font-bold  text-base text-slate-100">
                   {project.title}
                 </h3>
@@ -53,7 +56,9 @@ export function Projects() {
                     {project.description}
                   </span>
                 </div>
-                <div className="flex flex-1 rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500" />
+                <div>
+                  <Image src={"/projects/"+project.image} width={1000} height={1000} alt="thumbnail" className="flex flex-1 rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500 border-2 border-transparent" />
+                </div>
               </div>
             </PinContainer>
             {/* <div className="mt-10">Hi</div> */}
