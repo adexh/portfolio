023cf710257e4a49d33cc1bbe53790885b69835c
Adesh Tamrakar <adesht@outlook.com>
2024-03-03 22:35:47 +0530

    Added selector

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,7 @@ module.exports = {
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  darkMode: "class",
+  darkMode: "selector",
   theme: {
     extend: {
       animation: {
