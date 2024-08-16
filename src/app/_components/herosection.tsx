32b761a0e2943b00776a6e211c933c967c316b12
Adesh Tamrakar <adesht@outlook.com>
2024-08-16 21:40:10 +0530

    Cosmetic changes

--- src/app/_components/herosection.tsx
+++ src/app/_components/herosection.tsx
@@ -1,26 +1,27 @@
 import { Meteors } from "@/components/ui/meteors";
-
-const exp = () => {
-  var inputDate = new Date('07-07-2021');
-  var currentDate = new Date();
-  var timeDiff = currentDate.getTime() - inputDate.getTime();
-  var years = timeDiff / (1000 * 3600 * 24 * 365.25);
-  var roundedYears = parseFloat(years.toFixed(1));
-  return roundedYears;
-}
+import ResumeButton from "./resumeButton";
 
 export default function Herosection() {
+  const exp = () => {
+    const inputDate = new Date('07-07-2021');
+    const currentDate = new Date();
+    const timeDiff = currentDate.getTime() - inputDate.getTime();
+    const years = timeDiff / (1000 * 3600 * 24 * 365.25);
+    const roundedYears = parseFloat(years.toFixed(1));
+    return roundedYears;
+  }
+
   return (
     <div className="relative overflow-hidden">
       <Meteors className="" number={40} />
       <div className="mt-40 ml-5 sm:ml-10">
-        <h1 className="text-6xl md:text-7xl font-bold text-white ">
+        <h1 className="text-6xl md:text-6xl font-bold text-white">
           Hi,<br />I am Adesh <span className="sm:inline hidden">Tamrakar</span>
         </h1>
-        <h2 className="text-4xl mt-14">
-          Full-Stack Developer
+        <h2 className="text-4xl mt-4 sm:flex sm:justify-start sm:items-center">
+          <div>Full-Stack Developer<div className="my-2 text-2xl">Experience - {exp()}+ years</div></div>
+          <div className="sm:ml-8"><ResumeButton /></div>
         </h2>
-        <h3 className="my-2 text-lg">Experience - {exp()}+ years</h3>
       </div>
     </div>
   )
