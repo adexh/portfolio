1b5a84c0acd670fda996848ec08d741b41d89d88
Adesh Tamrakar <adesht@outlook.com>
2025-11-09 17:09:59 +0530

    Removed resume download button

--- src/components/ui/resumeButton.tsx
+++ src/components/ui/resumeButton.tsx
@@ -4,10 +4,9 @@ import { BackgroundGradient } from "@/components/ui/background-gradient";
 export default function ResumeButton() {
     return (
         <div>
-            <button className="my-8 sm:my-10 w-32 sm:w-auto sm:mr-5" onClick={() => window.open('https://docs.google.com/document/d/1uvy0ucEqqrBF_8RC4JVTRA-oOrPUxJ4JJ1s3Z3sOpYA/export?format=pdf')}>
-                <BackgroundGradient className="text-2xl font-mono h-auto rounded-[22px] px-4 py-2 bg-transparent bg-zinc-900">Resume</BackgroundGradient>
+            <button className="my-8 sm:my-10 w-32 sm:w-auto sm:mr-5" onClick={() => window.open('mailto:adesh.t@outlook.com')}>
+                <BackgroundGradient className="text-2xl font-mono h-auto rounded-[22px] px-4 py-2 bg-transparent bg-zinc-900">Contact</BackgroundGradient>
             </button>
         </div>
-
     )
 }
